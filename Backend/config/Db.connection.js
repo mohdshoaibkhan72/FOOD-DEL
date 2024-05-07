1d341b4aa1c9d34903a863c194b189b4dbc6d295
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-05-07 13:21:43 +0530

    just setup  for back end

---
+++ Backend/config/Db.connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION).then(() => {
+      console.log("Database connected successfully");
+    });
+  } catch (error) {
+    console.log("The error is ", error);
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,17 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/Db.connection.js";
+
+//app config
 const app = express();
 const port = 3000;
 
+//midlewear
+app.use(express.json());
+app.use(cors());
+
+//db connection
+connectDB();
+
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server in reunning at Port : ${port}!`));
