07305cfe9dfb12c7eea2cf80cd2f1606d9da8e95
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-05-09 10:22:21 +0530

    Login Api

---
+++ Backend/controllers/Login.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const User = require("../models/Registration.models");
+const bcrypt = require("bcrypt");
+const app = express();
+
+const Login = async (req, res) => {
+  //
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Please provide all fields" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User not found, please register" });
+    }
+
+    if (existingUser.password !== password) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Incorrect password" });
+    }
+
+    // Login successful
+    console.log("Login successful");
+    return res.status(200).send({ success: true, message: "Login successful" });
+  } catch (error) {
+    console.error("Error:", error);
+    return res
+      .status(500)
+      .send({ success: false, message: "Internal server error" });
+  }
+};
+
+module.exports = Login;
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const connectDB = require("./config/Db.connection");
 const Registeruser = require("./controllers/Register");
+const Login = require("./controllers/Login");
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,8 @@ app.use(express.json());
 
 //Register Login
 app.post("/register", Registeruser);
+app.post("/login", Login);
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Server is running at Port : ${port}!`));
